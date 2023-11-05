f31e9d654eaa09453f3320df9860242fa6d0a99f
Khang Duy <nguyenkhangbn8501@gmail.com>
2023-11-05 12:28:27 +0700

    'update'

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
 import App from './src';
-const port: number | string = process.env.PORT || 3000;
+const port: number | string = process.env.PORT || 4000;
 const myApp = new App(port);
 myApp.start();
